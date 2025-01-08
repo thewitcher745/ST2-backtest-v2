6f8fcfe60323e9483982f749ef2575323fa15b93
thewitcher745 <thewitcher745@gmail.com>
2025-01-08 05:35:19 +0330

    FEAT: Added plotting using lightweight_charts With dynamic marker placement for MSB points

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from lightweight_charts import Chart
 
 import utils.datatypes as dt
 
@@ -23,6 +24,62 @@ def load_local_data(pair_name: str = "BTCUSDT", timeframe: str = "15m") -> dt.Pa
     pair_df['candle_color'] = pair_df.apply(lambda row: 'green' if row.close > row.open else 'red', axis=1)
 
     return dt.PairDf(pair_df)
+
+
+class PlottingTool:
+    def __init__(self):
+        self.chart = Chart()
+        self.chart.legend(visible=True, ohlc=True, color_based_on_candle=True, font_size=15)
+
+        self.pair_df: dt.PairDf | None = None
+
+    def register_msb_point_updates(self, msb_points_df: dt.MSBPointsDf):
+        # Subscribe to the even of the chart's range change
+        self.chart.events.range_change += lambda chart, bars_before, bars_after: self.update_markers_on_range_change(chart,
+                                                                                                                     bars_before,
+                                                                                                                     bars_after,
+                                                                                                                     msb_points_df)
+
+    def update_markers_on_range_change(self, chart, bars_before, bars_after, msb_points_df: dt.MSBPointsDf):
+        # Clear all the markers
+        self.chart.clear_markers()
+
+        if bars_before < 0:
+            first_bar_pdi = self.pair_df.iloc[0].name
+        else:
+            first_bar_pdi = self.pair_df.iloc[int(bars_before)].name
+
+        if bars_after < 0:
+            last_bar_pdi = self.pair_df.iloc[-1].name
+        else:
+            last_bar_pdi = self.pair_df.iloc[-int(bars_after)].name
+
+        msb_points_in_range = msb_points_df[(msb_points_df.pdi >= first_bar_pdi) & (msb_points_df.pdi <= last_bar_pdi)]
+
+        self.draw_msb_points(msb_points_in_range)
+        print(len(self.chart.markers))
+
+    def draw_candlesticks(self, pair_df: dt.PairDf):
+        self.pair_df = pair_df
+        self.chart.set(pair_df)
+
+    def draw_zigzag(self, zigzag_df: dt.ZigZagDf):
+        line = self.chart.create_line('pivot_value')
+        line.set(zigzag_df[['time', 'pivot_value']])
+
+    def draw_msb_points(self, msb_points_df: dt.MSBPointsDf):
+        plotting_df = msb_points_df.sort_values(by=['pdi'])
+        for _, msb_point in msb_points_df.iterrows():
+            time = self.pair_df.iloc[msb_point.pdi].time
+            if msb_point.type == 'long':
+                marker = self.chart.marker(time, position='above', shape='arrow_up', color='green')
+            else:
+                marker = self.chart.marker(time, position='below', shape='arrow_down', color='red')
+
+    # def update_msb_points(self, msb_points_df: dt.MSBPointsDf):
+
+    def show(self):
+        self.chart.show(block=True)
 
 
 def convert_timestamp_to_readable(timestamp: pd.Timestamp) -> str:
